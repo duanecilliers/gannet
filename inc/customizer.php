f4e1d62b1ec49b6651557944ae7bc1bc1c2a6b1a
Duane Cilliers <duanecilliers@gmail.com>
2015-01-30 15:36:13 +0200

    create options for typography

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,61 @@ function gannet_customize_preview_js() {
 	wp_enqueue_script( 'gannet_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'gannet_customize_preview_js' );
+
+function gannet_customize_options() {
+
+	// Theme defaults
+	$primary_color = '#5bc08c';
+	$secondary_color = '#666';
+
+	// Stores all the controls that will be added
+	$options = array();
+
+	// Stores all the sections to be added
+	$sections = array();
+
+	// Stores all the panels to be added
+	$panels = array();
+
+	// Adds the sections to the $options array
+	$options['sections'] = $sections;
+
+	/**
+	 * Typography
+	 */
+	$section = 'typography';
+	$font_choices = customizer_library_get_font_choices();
+
+	$sections[] = array(
+		'id' => $section,
+		'title' => __( 'Typography', 'gannet' ),
+		'priority' => '80'
+	);
+
+	$options['primary-font'] = array(
+		'id' => 'primary-font',
+		'label'   => __( 'Primary Font', 'gannet' ),
+		'section' => $section,
+		'type'    => 'select',
+		'choices' => $font_choices,
+		'default' => 'Quattrocento'
+	);
+	$options['secondary-font'] = array(
+		'id' => 'secondary-font',
+		'label'   => __( 'Secondary Font', 'gannet' ),
+		'section' => $section,
+		'type'    => 'select',
+		'choices' => $font_choices,
+		'default' => 'Fanwood Text'
+	);
+
+	// Adds the sections to the $options array
+	$options['sections'] = $sections;
+
+	// Adds the panels to the $options array
+	$options['panels'] = $panels;
+
+	$customizer_library = Customizer_Library::Instance();
+	$customizer_library->add_options( $options );
+}
+add_action( 'init', 'gannet_customize_options' );
