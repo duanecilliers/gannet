4bf92e7f8e548e5691d2e88112cd5dd1fb39bb03
Duane Cilliers <duanecilliers@gmail.com>
2015-01-30 15:17:28 +0200

    initial commit

---
+++ content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Gannet
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php gannet_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'gannet' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php gannet_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Gannet
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'gannet' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'gannet' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'gannet' ), 'Gannet', '<a href="http://duane.co.za" rel="designer">Duane Cilliers</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+d
+<?php wp_footer(); ?>
+
+</body>
+</html>
