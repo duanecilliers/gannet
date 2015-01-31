b671e2a0b97f6198ca18369448e9339cbf496de4
Duane Cilliers <duanecilliers@gmail.com>
2015-01-31 13:56:25 +0200

    apply default background image

--- content-quote.php
+++ content-quote.php
@@ -5,6 +5,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<div class="entry-meta">
+			<?php gannet_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 
