44c43fe30a25a367bc125cd74e997a04a015f7e9
Duane Cilliers <duanecilliers@gmail.com>
2016-01-14 15:57:33 +0200

    move header_image section into header panel and add logo section to header panel

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,6 +14,39 @@ function gannet_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+  // Kint::dump($GLOBALS, $_SERVER); // pass any number of parameters
+  // ddd($wp_customize);
+
+  Kirki::add_config( 'gannet_config', array(
+    'capability'  => 'edit_theme_options',
+    'option_type' => 'theme_mod'
+  ) );
+
+  Kirki::add_panel( 'header_panel', array(
+    'priority'    => 70,
+    'title'       => __( 'Header', 'gannet' )
+  ) );
+
+  Kirki::add_section( 'logo', array(
+    'priority'    => 70,
+    'title'       => __( 'Header Logo', 'gannet' ),
+    'description' => __( '<p class="customizer-section-intro">While you can crop images to your liking after clicking <strong>Add new image</strong, your theme recommends a maximum logo size of 260 x 80 pixels. The retina version is doulbe that resolution, so 520 x 160 pixels.</p>', 'gannet' ),
+    'panel'       => 'header_panel'
+  ) );
+
+  /**
+   * Customise "Header Image" section
+   */
+  $wp_customize->get_section( 'header_image' )->panel = 'header_panel';
+
+  Kirki::add_field( 'gannet_config', array(
+    'settings'    => 'logo',
+    'label'       => __( 'Upload Your Logo', 'gannet' ),
+    'section'     => 'logo',
+    'type'        => 'upload'
+  ) );
+
 }
 add_action( 'customize_register', 'gannet_customize_register' );
 
