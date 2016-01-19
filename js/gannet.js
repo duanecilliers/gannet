8dc4f8e1c9290bd77cff6ec21887c4c5a86ffabc
Duane Cilliers <duanecilliers@gmail.com>
2016-01-19 17:10:55 +0200

    dynamic masonry gutter sizing

--- js/gannet.js
+++ js/gannet.js
@@ -3,12 +3,13 @@
   var $grid = $('.masonry-grid #main');
 
   $grid.prepend('<div class="grid-sizer"></div>');
+  $grid.prepend('<div class="gutter-sizer"></div>');
 
   var $masonry = $grid.masonry({
     itemSelector: '.hentry',
     columnWidth: '.grid-sizer',
     percentPosition: true,
-    gutter: 20
+    gutter: '.gutter-sizer'
   });
 
   $grid.imagesLoaded().progress(function () {
