92bc20a81deecd38648ac5825017081e1f3ffcfa
Duane Cilliers <duanecilliers@gmail.com>
2016-01-19 16:10:45 +0200

    include jquery masonry

--- functions.php
+++ functions.php
@@ -120,6 +120,10 @@ function gannet_scripts() {
 
 	wp_enqueue_script( 'gannet-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	if ( is_archive() || is_home() ) {
+		wp_enqueue_script( 'jquery-masonry', get_template_directory_uri() . '/js/masonry.pkgd.js', array( 'jquery' ), '4.0.0', true );
+	}
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
