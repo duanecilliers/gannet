9654f6e33411ad399a60046b433860171c4ef668
Duane Cilliers <duanecilliers@gmail.com>
2016-01-20 17:40:25 +0200

    add post thumb image sizes

--- functions.php
+++ functions.php
@@ -42,6 +42,9 @@ function gannet_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 
+	add_image_size( 'post-thumb', 300, 200, true );
+	add_image_size( 'post-thumb-lg', 780, 520, true );
+
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'gannet' ),
