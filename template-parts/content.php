9c96f99a9697fa6ca3f8c7144b940f785d24589f
Duane Cilliers <duanecilliers@gmail.com>
2016-01-20 18:27:05 +0200

    replace content with excerpt when not on single page

--- template-parts/content.php
+++ template-parts/content.php
@@ -40,11 +40,15 @@
 
 		<div class="entry-content">
 			<?php
-				the_content( sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'gannet' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
+				if ( is_single() ) {
+					the_content( sprintf(
+						/* translators: %s: Name of current post. */
+						wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'gannet' ), array( 'span' => array( 'class' => array() ) ) ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					) );
+				} else {
+					the_excerpt();
+				}
 
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gannet' ),
